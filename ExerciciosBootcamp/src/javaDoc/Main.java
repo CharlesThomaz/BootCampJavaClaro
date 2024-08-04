eba9ae7db3074756c639a115d002c2a88699d447
Charles Thomaz <138392103+CharlesThomaz@users.noreply.github.com>
2024-08-04 18:26:44 -0300

    UPDATE

---
+++ ExerciciosBootcamp/src/javaDoc/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+    String nome = args [0];
+    String sobrenome = args [1];
+        System.out.println(nome + sobrenome);
+
+    }
+}
